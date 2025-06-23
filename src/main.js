08336c026582283c66e5627cd14370ce587433cc
Arthur Vitor <arhturvitorsilvio@gmail.com>
2025-06-23 19:51:58 -0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,4 @@
+import './assets/main.css' // importei o css
+import { createApp } from 'vue' // importei a funcao createApp do pacote vue
+import App from './App.vue' //importei o componente
+createApp(App).mount('#app') // exibi o meu componente (RAIZ) a partir da DIV com id app no index
